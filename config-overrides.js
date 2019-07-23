ac49accbfd13c4e53ae0eece4519ae043eabdbc9
tainh5 <52400269+tainh5@users.noreply.github.com>
2019-07-23 11:36:16 +0700

    add alias for config

--- config-overrides.js
+++ config-overrides.js
@@ -1,24 +1,34 @@
-const {override, fixBabelImports, addLessLoader, disableEsLint} = require('customize-cra');
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  disableEsLint,
+  addWebpackAlias
+} = require("customize-cra");
+const path = require("path");
 
 module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true,
-    }),
-    disableEsLint(),
-    addLessLoader({
-        javascriptEnabled: true,
-        modifyVars: {
-            "@primary-color": "#1890ff",
-            "@table-padding-vertical": "6px",
-            "@table-padding-horizontal": "8px",
-            "@link-color": "#1890ff",
-            "@font-family": "'Helvetica Neue', Arial, sans-serif",
-            "@form-item-margin-bottom": "0px",
-            "@animation-duration-slow": "0s",
-            "@animation-duration-base": "0s",
-            "@animation-duration-fast": "0s",
-        },
-    }),
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
+  }),
+  addWebpackAlias({
+    ["~"]: path.resolve(__dirname, "src")
+  }),
+  disableEsLint(),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      "@primary-color": "#1890ff",
+      "@table-padding-vertical": "6px",
+      "@table-padding-horizontal": "8px",
+      "@link-color": "#1890ff",
+      "@font-family": "'Helvetica Neue', Arial, sans-serif",
+      "@form-item-margin-bottom": "0px",
+      "@animation-duration-slow": "0s",
+      "@animation-duration-base": "0s",
+      "@animation-duration-fast": "0s"
+    }
+  })
 );
